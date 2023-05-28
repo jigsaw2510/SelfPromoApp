d2669f609967df13a9698e9f603d8d286e4c4503
Catalin Gavrila <cata_bb92@yahoo.com>
2023-05-29 00:13:42 +0300

    implemented implicit intent for opening sms app

--- app/src/main/java/com/example/selfpromoapp/PreviewActivity.kt
+++ app/src/main/java/com/example/selfpromoapp/PreviewActivity.kt
@@ -1,32 +1,52 @@
 package com.example.selfpromoapp
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_preview.button_send_message
 import kotlinx.android.synthetic.main.activity_preview.text_view_message
 
 class PreviewActivity : AppCompatActivity() {
 
+    private lateinit var message: Message
+    private lateinit var messagePreviewText: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_preview)
-        val message = intent.getSerializableExtra("Message") as Message
-        val messagePreviewText = """
-            Hi ${message.contactName},
-            
-            My name is ${message.myDisplayName} and I am ${message.getFullJobDescription()}.
-            
-            I have a portfolio of apps to demonstrate my technical skills that I can show on request.
-            
-            I am able to start a new position ${message.getAvailability()}.
-            
-            Please get in touch if you have any suitable roles for me.
-            
-            Thanks and best regards.
-            
-        """.trimIndent()
+        displayMessage()
+        setupButton()
+    }
+
+    private fun displayMessage() {
+        message = intent.getSerializableExtra("Message") as Message
+        messagePreviewText = """
+                Hi ${message.contactName},
+                
+                My name is ${message.myDisplayName} and I am ${message.getFullJobDescription()}.
+                
+                I have a portfolio of apps to demonstrate my technical skills that I can show on request.
+                
+                I am able to start a new position ${message.getAvailability()}.
+                
+                Please get in touch if you have any suitable roles for me.
+                
+                Thanks and best regards.
+                
+            """.trimIndent()
 
         text_view_message.text = messagePreviewText
     }
 
+    private fun setupButton() {
+        button_send_message.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse("smsto: ${message.contactNumber}")  // Only SMS apps respond to this.
+                putExtra("sms_body", messagePreviewText)
+            }
+            startActivity(intent)
+        }
+    }
 
 }
